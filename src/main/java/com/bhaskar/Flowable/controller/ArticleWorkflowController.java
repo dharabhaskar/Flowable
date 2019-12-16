97a351dc75e99198204a8d5bd12eb2fe32b7d05c
Bhaskar Dhara <dharabhaskar@gmail.com>
2019-12-16 19:58:17 +0530

    -- article review workflow implemented partially.

---
+++ src/main/java/com/bhaskar/Flowable/controller/ArticleWorkflowController.java
@@ -0,0 +1,29 @@
+package com.bhaskar.Flowable.controller;
+
+import com.bhaskar.Flowable.domain.Approval;
+import com.bhaskar.Flowable.domain.Article;
+import com.bhaskar.Flowable.service.ArticleWorkflowService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+public class ArticleWorkflowController {
+    @Autowired
+    private ArticleWorkflowService service;
+    @PostMapping("/submit")
+    public void submit(@RequestBody Article article) {
+        service.startProcess(article);
+    }
+    @GetMapping("/tasks")
+    public List<Article> getTasks(@RequestParam String assignee) {
+        return service.getTasks(assignee);
+    }
+    @PostMapping("/review")
+    public void review(@RequestBody Approval approval) {
+        service.submitReview(approval);
+    }
+}
--- src/test/java/com/bhaskar/Flowable/FlowableApplicationTests.java
+++ src/test/java/com/bhaskar/Flowable/FlowableApplicationTests.java
@@ -1,13 +1,43 @@
 package com.bhaskar.Flowable;
 
+import static org.flowable.engine.impl.test.AbstractTestCase.assertEquals;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
+import org.flowable.engine.test.Deployment;
+import org.flowable.spring.impl.test.FlowableSpringExtension;
+import org.flowable.task.api.Task;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ExtendWith(FlowableSpringExtension.class)
 @SpringBootTest
 class FlowableApplicationTests {
 
+	@Autowired
+	private RuntimeService runtimeService;
+	@Autowired
+	private TaskService taskService;
 	@Test
-	void contextLoads() {
+	@Deployment(resources = { "processes/article-workflow.bpmn20.xml" })
+	void articleApprovalTest() {
+		Map<String, Object> variables = new HashMap<String, Object>();
+		variables.put("author", "test@baeldung.com");
+		variables.put("url", "http://baeldung.com/dummy");
+		runtimeService.startProcessInstanceByKey("articleReview", variables);
+		Task task = taskService.createTaskQuery()
+				.singleResult();
+		assertEquals("Review the submitted tutorial", task.getName());
+		variables.put("approved", true);
+		taskService.complete(task.getId(), variables);
+		assertEquals(0, runtimeService.createProcessInstanceQuery()
+				.count());
 	}
 
 }
